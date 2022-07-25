28408d3768a7ee6f006516b9bb1e5dd0b1a6c2eb
Phillip Luther <dev@phillipluther.com>
2022-07-25 07:21:53 -0700

    Refactor remaining components to generic HTML

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,38 +1,36 @@
-import { Box, Flex, VStack, Container, VisuallyHidden, Text, HStack } from '@chakra-ui/react';
+import { VisuallyHidden } from 'react-aria';
 import Padder from './padder';
 import PrimaryNav from './primary-nav';
 import SocialMenu from './social-menu';
 
 const Footer = () => (
-  <Padder as="footer" bg="gray.800" color="gray.300">
-    <VisuallyHidden as="h2">Site Footer</VisuallyHidden>
-    <VStack align="center" spacing="0">
-      <Flex as="section">
-        <VisuallyHidden as="h3">Supplemental Navigation</VisuallyHidden>
-        <HStack marginTop="500">
-          <PrimaryNav onDark />
-        </HStack>
-      </Flex>
-      <Flex as="section" color="brand.700">
-        <VisuallyHidden as="h3">Never Miss a Post!</VisuallyHidden>
-        <SocialMenu onDark />
-      </Flex>
+  <Padder as="footer">
+    <VisuallyHidden elementType="h2">Site Footer</VisuallyHidden>
 
-      <Padder noX noBottom>
-        <Container layerStyle="textBlock" fontSize="sm" as="section">
-          <VisuallyHidden as="h3">Legal Information and Disclaimers</VisuallyHidden>
-          <Text>
-            All Principled Engineer content is Copyright &copy; {new Date().getFullYear()} by
-            Phillip Luther unless otherwise specified.
-          </Text>
-          <Text>
-            The opinions expressed on the Principled Engineer belong to me, Phillip Luther, and do
-            not necessarily reflect the views or opinions of any associated organizations or
-            corporate entities.
-          </Text>
-        </Container>
-      </Padder>
-    </VStack>
+    <section>
+      <VisuallyHidden elementType="h3">Supplemental Navigation</VisuallyHidden>
+      <nav>
+        <PrimaryNav onDark />
+      </nav>
+    </section>
+
+    <section>
+      <VisuallyHidden elementType="h3">Never Miss a Post!</VisuallyHidden>
+      <SocialMenu onDark />
+    </section>
+
+    <section>
+      <VisuallyHidden elementType="h3">Legal Information and Disclaimers</VisuallyHidden>
+      <p>
+        All Principled Engineer content is Copyright &copy; {new Date().getFullYear()} by Phillip
+        Luther unless otherwise specified.
+      </p>
+      <p>
+        The opinions expressed on the Principled Engineer belong to me, Phillip Luther, and do not
+        necessarily reflect the views or opinions of any associated organizations or corporate
+        entities.
+      </p>
+    </section>
   </Padder>
 );
 
